b36b6b19e6a37dced614d219b4635a6a05f99417
Danial Faisal <dfaisal@unomaha.edu>
2018-09-19 13:52:19 -0500

    additional files and changes for Heroku

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -4,11 +4,12 @@ from django.contrib import admin
 from .models import Post
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    exclude = ('slug',)
-    list_display = ('title', 'status', 'author', 'created', 'updated')
-    list_filter = ('status',)
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
     search_fields = ('title', 'body')
-
-
-admin.site.register(Post, PostAdmin)
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
